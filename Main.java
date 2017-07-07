9338b891b8f2bf7fb5feccbace3d167dc0cc17e9
keerthi1997 <keerthana.vjps@gmail.com>
2017-07-07 14:58:34 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Main
+{
+  public static void main(String[] args)throws IOException
+  {
+          int n,i;
+          BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+          n=Integer.parseInt(br.readLine());
+          //if(n%2==0)
+          for(i=1;i<=n;i++)
+          {
+            if(i%2==0)
+            System.out.println(i);
+            }
+            }
+            
